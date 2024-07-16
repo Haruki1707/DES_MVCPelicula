565c8a08f277d9fca5dfcf5983f2aed2c223d6f8
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2024-07-16 15:49:07 -0600

    Initial commit

---
+++ MVCPelicula/Models/PeliculasDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCPelicula.Models
+{
+    public class PeliculasDBContext : DbContext
+    {
+        public PeliculasDBContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Pelicula> Peliculas { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
